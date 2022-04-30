9956b08b99d835865c1ae57e765936f5c592828a
SomePineaple <njweb@protonmail.com>
2022-04-30 19:08:08 -0400

    Normals

--- src/ray.rs
+++ src/ray.rs
@@ -18,19 +18,24 @@ impl Ray {
     }
 
     pub fn color(&self) -> Color {
-        if self.hit_sphere(point3(0.0, 0.0, -1.0), 0.5) {
-            return vec3(1.0, 0.0, 0.0);
+        let mut t = self.hit_sphere(point3(0.0, 0.0, -1.0), 0.5);
+        if t > 0.0 {
+            let n = (self.at(t) - point3(0.0, 0.0, -1.0)).normalize();
+            return 0.5 * vec3(n.x + 1.0, n.y + 1.0, n.z + 1.0);
         }
-        let t = 0.5 * (self.dir.normalize().y + 1.0);
+        t = 0.5 * (self.dir.normalize().y + 1.0);
         (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0)
     }
 
-    fn hit_sphere(&self, center: Point3d, radius: f64) -> bool {
+    fn hit_sphere(&self, center: Point3d, radius: f64) -> f64 {
         let oc = self.origin - center;
         let a = dot(self.dir, self.dir);
         let b = 2.0 * dot(oc, self.dir);
         let c = dot(oc, oc) - radius*radius;
         let discriminant = b*b - 4.0*a*c;
-        return discriminant > 0.0;
+        if discriminant < 0.0 {
+            return -1.0;
+        }
+        (-b - discriminant.sqrt()) / (2.0 * a)
     }
 }
