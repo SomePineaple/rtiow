a2cdbce3953afa64815ccb68ea8248e5b69f15dc
SomePineaple <njweb@protonmail.com>
2022-04-30 18:47:16 -0400

    Gradient background

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
+use crate::utils::{Point3, Ray};
 use cgmath::vec3;
 use std::fs;
 
 mod utils;
 
-const IMAGE_WIDTH: i32 = 256;
-const IMAGE_HEIGHT: i32 = 256;
+const IMAGE_WIDTH: i32 = 400;
+const ASPECT_RATIO: f64 = 16.0 / 9.0;
+const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+
+const ORIGIN: Point3 = vec3(0.0, 0.0, 0.0);
 
 fn main() {
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let horizontal = vec3(viewport_width, 0.0, 0.0);
+    let vertical = vec3(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        ORIGIN - horizontal / 2.0 - vertical / 2.0 - vec3(0.0, 0.0, focal_length);
+
     let mut output = String::new();
     output += format!("P3\n{} {}\n256\n", IMAGE_WIDTH, IMAGE_HEIGHT).as_str();
     let mut j = IMAGE_HEIGHT - 1;
     while j >= 0 {
         for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let r = Ray::new(
+                ORIGIN,
+                lower_left_corner + u * horizontal + v * vertical - ORIGIN,
+            );
 
-            output += utils::color_str(vec3(r, g, b)).as_str();
+            output += utils::color_str(r.color()).as_str();
         }
 
         j -= 1;
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,8 @@
-use cgmath::Vector3;
+use cgmath::{InnerSpace, Vector3, vec3};
+
+pub type Point3 = Vector3<f64>;
+pub type Direction = Vector3<f64>;
+pub type Color = Vector3<f64>;
 
 pub fn to_byte_rgb(color: f64) -> u8 {
     (255.999 * color) as u8
@@ -11,4 +15,24 @@ pub fn color_str(color: Vector3<f64>) -> String {
         to_byte_rgb(color.y),
         to_byte_rgb(color.z)
     );
+}
+
+pub struct Ray {
+    origin: Vector3<f64>,
+    dir: Vector3<f64>,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, dir: Direction) -> Self {
+        Self { origin, dir }
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        return self.origin + t * self.dir;
+    }
+
+    pub fn color(&self) -> Color {
+        let t = 0.5 * (self.dir.normalize().y + 1.0);
+        (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0)
+    }
 }
